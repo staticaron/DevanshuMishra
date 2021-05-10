0db2362da661df07ab7e583d27f1b191f19ab382
Devanshu Mishra <mishradev1910@gmail.com>
2021-05-10 19:53:36 +0000

    Menu Action
    Signed-off-by: Devanshu Mishra <mishradev1910@gmail.com>

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,19 @@
+let menuObj = document.getElementById("Menu");
 
+function ToggleVisibilityMenu() {
+    if (menuObj.display == "none") {
+        OpenMenu();
+    } else {
+        CloseMenu();
+    }
+
+    alert("Toggled");
+}
+
+function OpenMenu() {
+    menuObj.style.display = "none";
+}
+
+function CloseMenu() {
+    menuObj.style.display = "none";
+}
