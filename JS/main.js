c22aedf393b39aee8221abd005add55790b72e8a
Devanshu Mishra <devm19102003@gmail.com>
2021-10-19 08:59:11 +0000

    Welcome Message updated again.

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,3 @@
-let menuObj = document.getElementById("Menu");
-
 function Loaded() {
     alert("Welcome. The website sucks ik.");
-}
-
-function ToggleVisibilityMenu() {
-    if (menuObj.display == "none") {
-        OpenMenu();
-    } else {
-        CloseMenu();
-    }
-
-    alert("Toggled");
-}
-
-function OpenMenu() {
-    menuObj.style.display = "none";
-}
-
-function CloseMenu() {
-    menuObj.style.display = "none";
 }
